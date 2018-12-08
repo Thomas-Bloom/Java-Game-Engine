469de2ce6b672b1a7c42cef5dc44e349f40922d2
Tom Bloom <thomasbloom25@gmail.com>
2018-12-08 00:00:54 +0000

    Frame update code, Window

---
+++ src/com/tombloom/javaengine/GameContainer.java
@@ -0,0 +1,28 @@
+package com.tombloom.javaengine;
+
+public class GameContainer implements Runnable{
+
+    private Thread thread;
+    private boolean isRunning;
+    private double updateCap = 1.0/60.0;
+
+    public GameContainer(){
+
+    }
+
+    public void start(){
+
+    }
+
+    public void stop(){
+
+    }
+
+    public void run(){
+
+    }
+
+    private void dispose(){
+
+    }
+}
